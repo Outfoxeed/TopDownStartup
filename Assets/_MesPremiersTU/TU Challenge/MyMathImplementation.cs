8eadcd93b6a7a1ffb0b69d74f64a16d4474ff6b2
Outfoxeed <letourneaumorigan@gmail.com>
2023-09-19 10:00:57 +0200

    String and math implementation

--- Assets/_MesPremiersTU/TU Challenge/MyMathImplementation.cs
+++ Assets/_MesPremiersTU/TU Challenge/MyMathImplementation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace TU_Challenge
 {
@@ -10,67 +11,144 @@ namespace TU_Challenge
     {
         internal static int Add(int a, int b)
         {
-            throw new NotImplementedException();
+            return a + b;
         }
 
-        internal static List<int> GenericSort(List<int> toSort, Func<int, int, bool> isInOrder)
+        // Destructive func
+        internal static List<int> GenericSort(List<int> toSort, Func<int, int, int> isInOrder)
         {
-            throw new NotImplementedException();
+            if (toSort == null)
+            {
+                throw new ArgumentException("Given list to sort is null");
+            }
+            if (isInOrder == null)
+            {
+                throw new ArgumentException("isInOrder comparer is null");
+            }
+            
+            for (int i = 0; i < toSort.Count; i++)
+            {
+                bool sorted = true;
+                for (int j = 0; j < toSort.Count - 1; j++)
+                {
+                    int comparison = isInOrder.Invoke(toSort[j], toSort[j + 1]);
+                    if (comparison == -1)
+                    {
+                        sorted = false;
+                        (toSort[j], toSort[j + 1]) = (toSort[j + 1], toSort[j]);
+                    }
+                }
+
+                if (sorted)
+                {
+                    break;
+                }
+            }
+
+            return toSort;
         }
 
         internal static List<int> GetAllPrimary(int a)
         {
-            throw new NotImplementedException();
+            List<int> results = new();
+            for (int i = 1; i <= a; i++)
+            {
+                if (!IsPrimary(i))
+                {
+                    continue;
+                }
+                
+                results.Add(i);
+            }
+            return results;
         }
 
         internal static bool IsDivisible(int a, int b)
         {
-            throw new NotImplementedException();
+            return a % b == 0;
         }
 
         internal static bool IsEven(int a)
         {
-            throw new NotImplementedException();
+            return a % 2 == 0;
         }
 
-        internal static bool IsInOrder(int a, int b)
+        internal static int IsInOrder(int a, int b)
         {
-            throw new NotImplementedException();
+            if (a == b)
+            {
+                return 0;
+            }
+            return a < b ? 1 : -1;
         }
 
-        internal static bool IsInOrderDesc(int arg1, int arg2)
+        internal static int IsInOrderDesc(int arg1, int arg2)
         {
-            throw new NotImplementedException();
+            return -IsInOrder(arg1, arg2);
         }
 
         internal static bool IsListInOrder(List<int> list)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < list.Count-1; i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    return false;
+                }   
+            }
+            return true;
         }
 
         internal static bool IsMajeur(int age)
         {
-            throw new NotImplementedException();
+            if (age < 0)
+            {
+                throw new ArgumentException("Given age is negative");
+            }
+
+            if (age > 140)
+            {
+                throw new ArgumentException("Given age is to high");
+            }
+            return age >= 18;
         }
 
         internal static bool IsPrimary(int a)
         {
-            throw new NotImplementedException();
+            if (a <= 1)
+            {
+                return false;
+            }
+            
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         internal static int Power(int a, int b)
         {
-            throw new NotImplementedException();
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            return result;
         }
 
         internal static int Power2(int a)
         {
-            throw new NotImplementedException();
+            return a * a;
         }
 
+        // Destructive method
         internal static List<int> Sort(List<int> toSort)
         {
-            throw new NotImplementedException();
+            return GenericSort(toSort, IsInOrder);
         }
     }
 }
--- Assets/_MesPremiersTU/TU Challenge/MyStringImplementation.cs
+++ Assets/_MesPremiersTU/TU Challenge/MyStringImplementation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace TU_Challenge
 {
@@ -15,12 +16,42 @@ namespace TU_Challenge
 
         internal static bool IsNullEmptyOrWhiteSpace(string input)
         {
-            throw new NotImplementedException();
+            if (input == null || input.Length == 0)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         internal static string MixString(string a, string b)
         {
-            throw new NotImplementedException();
+            if (IsNullEmptyOrWhiteSpace(a) || IsNullEmptyOrWhiteSpace(b))
+            {
+                throw new ArgumentException("Given string is null or empty");
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            int maxIndex = Mathf.Max(a.Length, b.Length);
+            for (int i = 0; i < maxIndex; i++)
+            {
+                if (i < a.Length)
+                {
+                    sb.Append(a[i]);
+                }         
+                if (i < b.Length)
+                {
+                    sb.Append(b[i]);
+                }
+            }
+            return sb.ToString();
         }
 
         internal static string ToCesarCode(string input, int offset)
@@ -30,7 +61,32 @@ namespace TU_Challenge
 
         internal static string ToLowerCase(string a)
         {
-            throw new NotImplementedException();
+            if (IsNullEmptyOrWhiteSpace(a))
+            {
+                throw new ArgumentException("Given string is null or empty");
+            }
+            
+            StringBuilder sb = new();
+            for (int i = 0; i < a.Length; i++)
+            {
+                sb.Append(ToLowerCase(a[i]));
+            }
+            return sb.ToString();
+        }
+
+        internal static char ToLowerCase(char character)
+        {
+            Dictionary<char, char> lowerCase = new() { { 'É', 'é' }, {'Ç', 'ç'}};
+            if (65 <= character && character <= 90)
+            {
+                return (char)(character + 32);
+            }
+
+            if (lowerCase.TryGetValue(character, out var lowerCaseCharacter))
+            {
+                return lowerCaseCharacter;
+            }
+            return character;
         }
 
         internal static string UnBazardString(string input)
@@ -40,7 +96,16 @@ namespace TU_Challenge
 
         internal static string Voyelles(string a)
         {
-            throw new NotImplementedException();
+            char[] voyels = { 'a', 'e', 'i', 'o', 'u' };
+            StringBuilder sb = new();
+            foreach (char character in a)
+            {
+                if (voyels.Contains(ToLowerCase(character)))
+                {
+                    sb.Append(character);
+                }
+            }
+            return sb.ToString();
         }
     }
 }

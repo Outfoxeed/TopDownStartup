cd6e53abd69b631567f89a6cdd42b2fd809f84dd
Outfoxeed <letourneaumorigan@gmail.com>
2023-09-20 11:59:25 +0200

    [ADD] Gun Base script

---
+++ Assets/_Scripts/Runtime/Guns/GunBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Game.Runtime.UpdateSystem;
+using Zenject;
+
+namespace Game.Runtime.Guns
+{
+    public abstract class GunBase : IGun
+    {
+        protected IDisposable _updateSubscription;
+
+        [Inject]
+        public virtual void Construct(IUpdateSystem updateSystem)
+        {
+            _updateSubscription = updateSystem.SubsribeToUpdate(this);
+        }
+        public void Deconstruct()
+        {
+            _updateSubscription.Dispose();
+            _updateSubscription = null;
+        }
+
+        public abstract void Shoot(IShooter source);
+        public abstract void Update(float deltaTime);
+    }
+}

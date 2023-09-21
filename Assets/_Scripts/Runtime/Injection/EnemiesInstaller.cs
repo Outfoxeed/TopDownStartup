dba3631af0482d3b3f4567ae8eebd90f6acfee50
Outfoxeed <letourneaumorigan@gmail.com>
2023-09-22 00:26:56 +0200

    [UPDATE] SceneContext prefab (Zenject bindings)

---
+++ Assets/_Scripts/Runtime/Injection/EnemiesInstaller.cs
@@ -0,0 +1,32 @@
+﻿using Game.Runtime.Enemies;
+using Game.Runtime.WaveSpawner;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Runtime.Injection
+{
+    public class EnemiesInstaller : MonoInstaller
+    {
+        [SerializeField] private AIBrain _enemyPrefab;
+        [SerializeField] private WaveSpawnerConfig _waveSpawnerConfig;
+        [SerializeField] private WavesData _wavesData;
+        
+        public override void InstallBindings()
+        {
+            // Prefabs
+            if (_enemyPrefab == null)
+            {
+                Debug.Log("uifehgiuheiufhui");
+            }
+            Container.Bind<AIBrain>().FromInstance(_enemyPrefab).AsSingle();
+            
+            // Scriptable objects
+            Container.Bind<WaveSpawnerConfig>().FromInstance(_waveSpawnerConfig).AsSingle();
+            Container.Bind<WavesData>().FromInstance(_wavesData).AsSingle();
+            
+            // Systems
+            Container.Bind<IEnemiesManager>().To<EnemiesManager>().AsSingle().NonLazy();
+            Container.Bind<IWaveSpawner>().To<WaveSpawner.WaveSpawner>().AsSingle().NonLazy();
+        }
+    }
+}
--- Assets/_Scripts/Runtime/Injection/InjectInstaller.cs
+++ Assets/_Scripts/Runtime/Injection/InjectInstaller.cs
@@ -1,4 +1,5 @@
 using System;
+using Game.Runtime.UpdateSystem;
 using UnityEngine;
 using Zenject;
 
@@ -6,9 +7,15 @@ namespace Game
 {
     public class InjectInstaller : MonoInstaller
     {
+        [SerializeField] private UpdateSystem _updateSystem;
+        [SerializeField] private PlayerReference _playerReference;
         public override void InstallBindings()
         {
+            // Scriptable
+            Container.Bind<PlayerReference>().FromInstance(_playerReference).AsSingle();
+            
             //Container.Bind<Patate>().AsSingle().NonLazy();
+            Container.Bind<IUpdateSystem>().FromComponentInNewPrefab(_updateSystem).AsSingle().NonLazy();
         }
     }
 }

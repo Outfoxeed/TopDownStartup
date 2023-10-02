3d6bd7108ed52ece2eadb14a4e5761e77a113c6d
Outfoxeed <letourneaumorigan@gmail.com>
2023-10-02 10:00:13 +0200

    [ADD] Combined Wave Commands

---
+++ Assets/_Scripts/Runtime/WaveSpawner/WaveCommands/CombinedCommand.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Game.Runtime.WaveSpawner.WaveCommands
+{
+    public class CombinedCommand : WaveCommand
+    {
+        public override bool IsFinished
+        {
+            get
+            {
+                for (int i = 0; i < _waveCommands.Length; i++)
+                {
+                    if (_waveCommands[i].IsFinished)
+                    {
+                        continue;
+                    }
+
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        [SerializeReference] private WaveCommand[] _waveCommands;
+        
+        public override void Init(IWaveSpawner waveSpawner)
+        {
+            for (var i = 0; i < _waveCommands.Length; i++)
+            {
+                var waveCommand = _waveCommands[i];
+                waveCommand.Init(waveSpawner);
+            }
+        }
+
+        public override void Update(float deltaTime)
+        {
+            for (var i = 0; i < _waveCommands.Length; i++)
+            {
+                var waveCommand = _waveCommands[i];
+                waveCommand.Update(deltaTime);
+            }
+        }
+    }
+}

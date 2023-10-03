6517997f5873751ac58aba66ddd22c34f96a445d
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-10-03 11:49:29 +0200

    [update]l evel design

---
+++ Assets/asset/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
@@ -0,0 +1,25 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    //when object exit the trigger, put it to the assigned layer and sorting layers
+    //used in the stair objects for player to travel between layers
+    public class LayerTrigger : MonoBehaviour
+    {
+        public string layer;
+        public string sortingLayer;
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            other.gameObject.layer = LayerMask.NameToLayer(layer);
+
+            other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = sortingLayer;
+            var srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach (var sr in srs) sr.sortingLayerName = sortingLayer;
+        }
+    }
+}
---
+++ Assets/asset/Pixel Art Top Down - Basic/Script/PropsAltar.cs
@@ -0,0 +1,36 @@
+﻿#region
+
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion
+
+//when something get into the alta, make the runes glow
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class PropsAltar : MonoBehaviour
+    {
+        public List<SpriteRenderer> runes;
+        public float lerpSpeed;
+
+        private Color curColor;
+        private Color targetColor;
+
+        private void Update()
+        {
+            curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+
+            foreach (var r in runes) r.color = curColor;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            targetColor = new Color(1, 1, 1, 1);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            targetColor = new Color(1, 1, 1, 0);
+        }
+    }
+}
